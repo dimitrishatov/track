0c2055919a3274f480c67df8ff980704e541ff34
bluesushi <kaanturhans@gmail.com>
2021-03-27 16:46:30 -0400

    incorporated everything

---
+++ cli/HttpHandler.java
@@ -0,0 +1,28 @@
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class HttpHandler {
+    private final static String base = "http://localhost:4567/";
+    private final static OkHttpClient client = new OkHttpClient();
+    private final Moshi moshi = new Moshi.Builder().build();
+    private final JsonAdapter<> gistJsonAdapter = moshi.adapter()
+
+    public static String getRequest(String url) throws IOException {
+        Request request = buildRequest(url);
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
+    }
+
+    private static Request buildRequest(String url) {
+        return new Request.Builder()
+                .url(base + url)
+                .build();
+    }
+}
---
+++ cli/TrackCli.java
@@ -0,0 +1,7 @@
+public class TrackCli {
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.initialize();
+    }
+}
