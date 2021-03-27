75fb080c05d42e03eed6a4e113a58f995b11a646
Dimitri Shatov <dimitri.113322@gmail.com>
2021-03-27 15:30:31 -0400

    small changes to room

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,6 +1,9 @@
 import java.util.UUID;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Room {
    private final UUID ROOM_KEY = UUID.randomUUID();
+   private ArrayList<User> users;
+   private HashMap<User, Integer> scores;
 }
