b8dbd0f3623d5814bb69f5f2f2aa6bcefa347ed9
Dimitri Shatov <dimitri.113322@gmail.com>
2021-03-27 15:38:16 -0400

    date added

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,9 +1,14 @@
 import java.util.UUID;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Date;
 
 public class Room {
    private final UUID ROOM_KEY = UUID.randomUUID();
    private ArrayList<User> users;
+   private ArrayList<Habit> habits;
    private HashMap<User, Integer> scores;
+   private Date endDate;
+
+
 }
