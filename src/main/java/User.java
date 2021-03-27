18661bfd632b287d037d2f121690dd5b6b30b9ec
bluesushi <kaanturhans@gmail.com>
2021-03-27 15:43:01 -0400

    added string field for users, username

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,10 @@
 import java.util.UUID;
 
 public class User {
-   private final UUID USER_ID = UUID.randomUUID();
+    private final UUID USER_ID = UUID.randomUUID();
+    private final String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
 }
